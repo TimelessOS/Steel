6bc2890b550d46f5dc3f7c4f4ef33b640f3cb516
Taxrosdev <taxrosdev@mailbox.org>
2025-09-18 16:16:45 +1200

    new: added curl, fixed musl compilation

--- common-scripts/autotools.sh
+++ common-scripts/autotools.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-export CC=musl-gcc
+export CC="musl-gcc -static"
 export CFLAGS="-O2 -fPIC"
 export LDFLAGS="-L$PWD/lib"
 export CPPFLAGS="-I$PWD/include"
 export PKG_CONFIG_PATH=$PWD/lib/pkgconfig:$PKG_CONFIG_PATH
+export PKG_CONFIG_LIBDIR=$PWD/lib/pkgconfig
+export PKG_CONFIG_SYSROOT_DIR=$PWD/
 
 # Generate ./configure
 if [ ! -x configure ]; then
@@ -24,9 +26,9 @@ fi
 
 export OUTDIR="$(pwd)/out"
 
-./configure --prefix=/ --libdir=/lib --disable-shared --enable-static "$@"
+./configure --verbose --prefix=/ --libdir=/lib --enable-static --with-pic --disable-runtime LIBS="-lssl -lcrypto" "$@"
 
-make STATIC=true STRIP=true
-make DESTDIR=$OUTDIR -j1 install
+make STATIC=true STRIP=true TARGET=x86_64-linux-musl
+make DESTDIR=$OUTDIR -j1 TARGET=x86_64-linux-musl install
 
 find $OUTDIR/lib -name '*.la' -delete
--- deps/openssl.sh
+++ deps/openssl.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
-./Configure --prefix=/ --openssldir=/etc/ssl linux-x86_64 no-shared --libdir=/lib
+export CC="musl-gcc -static"
+export CFLAGS="-O2 -fPIC"
+export LDFLAGS="-L$PWD/lib"
+export CPPFLAGS="-I$PWD/include"
+export PKG_CONFIG_PATH=$PWD/lib/pkgconfig:$PKG_CONFIG_PATH
+export PKG_CONFIG_LIBDIR=$PWD/lib/pkgconfig
+export PKG_CONFIG_SYSROOT_DIR=$PWD/
+
+./Configure --prefix=/ -fPIC threads no-shared --static -static --openssldir=/etc/ssl linux-x86_64 no-shared --libdir=/lib 
 make
 make DESTDIR=out install
